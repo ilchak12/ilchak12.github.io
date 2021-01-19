491395218f7622c4d56519033a9b70e393a16bc1
ilchak <ilchak12@gmail.com>
2021-01-19 22:12:22 +0200

    added product quantity js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Products count//
+
 let productsCountEl = document.getElementById("products-count");
 let btnAddToCart = document.querySelectorAll(".add-to-cart");
 
@@ -9,7 +11,7 @@ btnAddToCart.forEach(el => {
 
 
 ////////////////////////////////////////
-
+//Modal window//
 
 let btnMoreDetails = document.querySelectorAll(".detais");
 let modalWindow = document.querySelector(".modal");
@@ -44,10 +46,77 @@ let likeIcon = document.querySelectorAll(".heart-icon");
 
 likeIcon.forEach(el => {
     el.addEventListener("click", function() {
-        if(el.classList.contains("icon-active")) {
-            el.classList.remove("icon-active")
-        } else {
-            el.classList.add("icon-active");
-        }
+        el.classList.toggle("icon-active");
     })
 })
+
+///////////////////////////////////////////////
+//change product count
+
+let dekBtn = document.querySelectorAll(".dek-btn");
+let inkBtn = document.querySelectorAll(".ink-btn");
+let countValue = document.querySelectorAll(".product-quantity input");
+
+let minCount = 1;
+let maxCount = 5;
+
+function toggleBtn(count, i) {
+    dekBtn[i].disabled = count <= minCount;
+    inkBtn[i].disabled = count >= maxCount;
+}
+
+for(let i = 0; i < countValue.length; i++) {
+    let currentCount = +countValue[i].value;
+    toggleBtn(currentCount, i);
+}
+
+for(let i = 0; i < dekBtn.length; i++) {
+    dekBtn[i].addEventListener('click', function() {
+        let currentCount = +countValue[i].value;
+        let newCount = currentCount - 1;
+        countValue[i].value = newCount;
+
+        toggleBtn(newCount, i);
+    })
+}
+
+for(let i = 0; i < inkBtn.length; i++) {
+    inkBtn[i].addEventListener('click', function() {
+        let currentCount = +countValue[i].value;
+        let newCount = currentCount + 1;
+        countValue[i].value = newCount;
+
+        toggleBtn(newCount, i);
+    })
+}
+
+////
+
+// function Car(model, color, year) {
+//     this.model = model;
+//     this.color = color;
+//     this.year = year;
+// }
+
+// function Calculator(a, b) {
+//     this.a = a;
+//     this.b = b;
+    
+//     this.sum = function() {
+//         return this.a + this.b;
+//     }
+
+//     this.mul = function() {
+//         return this.a * this.b;
+//     }
+// }
+
+// let calc = new Calculator(5, 15);
+// console.log(calc.sum());
+
+// const audi = new Car("A4", "Black", 2015);
+// const bmw = new Car("328", "Gray", 2010);
+// console.log(audi);
+// console.log(bmw);
+
+
